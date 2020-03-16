97237fa6454fecbe81608bbd0e57ec26c90b7043
Michal Janočko <michal@janocko.com>
2020-03-16 18:01:03 +0100

    Final commit

--- main.js
+++ main.js
@@ -1,7 +1,26 @@
-let slovnik = [];
+const loading = document.getElementById('loading'),
+    word = document.getElementById('word'),
+    button = document.getElementById('generate');
+let dict;
 
-if (!localStorage['slovnik']) {
-    fetch('/slovnik.txt').then(response => {
-        slovnik = response.text();
-    });
-}
+const getWord = () => () => {
+    word.textContent = dict[Math.floor(
+        Math.random() * dict.length
+    )];
+};
+
+(async () => {
+    if (!localStorage['dict']) {
+        await fetch('/dict.txt').then(response => {
+            return response.text();
+        }).then(text => {
+            localStorage['dict'] = text;
+        });
+    }
+})().then(() => {
+    dict = localStorage['dict'].split('\n');
+    button.onclick = getWord();
+    button.click();
+});
+
+getWord();
